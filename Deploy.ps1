56622baccac878a8ee27c7795ba18305c978306d
ARJUN V <arjunvmm004@gmail.com>
2024-10-16 19:54:08 +0530

    added dockerfile and ps script

---
+++ Deploy.ps1
@@ -0,0 +1,37 @@
+
+#$projectPath = "./super-service"
+$imageName = "super-service:latest"
+$containerName = "super-service"
+$testPath = "./test"
+$dockerfilePath = "./Dockerfile"
+
+#Run the automated tests
+Write-Host "Running unit tests..."
+#Set-Location ./test
+dotnet test $testPath
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Unit tests failed. Review and fix." -ForegroundColor Red
+}
+
+# Build and package the application as a Docker image
+Write-Host "Building and Packaging the Application..."
+docker build -t $imageName -f $dockerfilePath .
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Docker build failed. Aborting!!" -ForegroundColor Red
+    exit 1
+}
+
+# Use Docker Scout to scan the image
+Write-Host "Scanning Image with Docker Scout..."
+docker scout cves $imageName
+if ($LASTEXITCODE -ne 0) {
+    Write-Host "Docker Scout found vulnerabilities. Review and fix." -ForegroundColor Yellow
+}
+
+#Deploy and run the image locally
+Write-Host "Deploying the Application Locally..."
+docker run -d --name $containerName -p 8080:80 $imageName
+
+docker ps
+
+Write-Host "Deployment complete" -ForegroundColor Green
---
+++ Dockerfile
@@ -0,0 +1,28 @@
+#Using Official .NET Docker image
+FROM mcr.microsoft.com/dotnet/sdk:3.1 AS build
+
+#Setting the Work Directory
+WORKDIR /super-svc
+
+#Copy csproj and restore dependencies
+COPY ./src/*.csproj .
+RUN dotnet restore
+
+#Copying the source-code and building
+COPY ./src .
+RUN dotnet publish -c Release -o /super-svc/publish
+
+#Using official runtime image
+FROM mcr.microsoft.com/dotnet/aspnet:3.1
+
+#Setting the Work Directory
+WORKDIR /super-svc
+
+#Copying from build stage
+COPY --from=build /super-svc/publish /super-svc/
+
+#Exposing the port (https://learn.microsoft.com/en-us/dotnet/core/compatibility/containers/8.0/aspnet-port)
+EXPOSE 80
+
+#Run the application
+ENTRYPOINT ["dotnet", "SuperService.dll"]
